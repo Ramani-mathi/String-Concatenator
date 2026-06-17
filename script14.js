f5924729cf44b9a3bd58bdaf14426301908c94c9
Ramani-mathi <monimathi1107@gmail.com>
2026-06-17 21:33:48 +0530

    Add files via upload

---
+++ script14.js
@@ -0,0 +1,6 @@
+function concatenate(){
+            var a = document.getElementById("str1").value;
+            var b = document.getElementById("str2").value;
+            var c = a + b;
+            document.getElementById("result").innerHTML= c;
+        }
